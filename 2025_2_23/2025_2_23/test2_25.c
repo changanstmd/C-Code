541fd794b4ecf1c9905db280b53df83b3b80c175
changanstmd <1209625009@qq.com>
2025-02-27 17:50:04 +0800

    2025_2_27

--- 2025_2_23/2025_2_23/test2_25.c
+++ 2025_2_23/2025_2_23/test2_25.c
@@ -108,31 +108,135 @@
 
 
 //方法二
-int is_prime(int n)
-{
-	int j = 0;
-	for (j = 2;j <= sqrt(n);j++)
-	{
-		if (n % j == 0)
-		{
-			return 0;
-		}	
-	}
-	return 1;
-}
+//bool类型
+//只能表示真和假
+#include <stdbool.h>//头文件
+//bool is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2;j <= sqrt(n);j++)
+//	{
+//		if (n % j == 0)
+//		{
+//			return false;//0也行
+//		}
+//	}
+//	return true;//1也行
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 101;i <= 200;i += 2)
+//	{
+//		if (is_prime(i))
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//	return 0;
+//}
 
+
+
+//打印1000~2000之间的闰年
+//闰年的判断规则
+//1.能被4整除，并且不能被100整除
+//2.能被400整除
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000;year <= 2000;year++)
+//	{
+//		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//写一个函数，实现一个整型有序数组的二分查找
+//int binary_search(int arr[], int k, int sz)
+////形参arr[]在这里不是数组，而是一个指针变量
+////数组传参实际上是传递的是数组首元素的地址
+////所以在一个函数内部计算一个函数参数大小是不靠谱的
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = left + (right - left) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	{
+//		if (ret == -1)
+//		{
+//			printf("找不到\n");
+//		}
+//		else
+//		{
+//			printf("找到了，下标是%d", ret);
+//		}
+//	}
+//	return 0;
+//}
+//形式参数和实际参数可以相同，也可以不同
+
+
+//写一个函数，每次调用这个函数，就会将num的值增加
+//方法一
+//void Add(int *p)
+//{
+//	(*p)++;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("%d\n", num);
+//	Add(&num);
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//方法二
+int Add(int p)
+{
+	return ++p;
+}
 int main()
 {
-	int i = 0;
-	int count = 0;
-	for (i = 101;i <= 200;i+=2)
-	{
-		if (is_prime(i))
-		{
-			printf("%d ", i);
-			count++;
-		}
-	}
-	printf("\ncount = %d\n", count);
+	int num = 0;
+	num = Add(num);
+	printf("%d\n", num);
+	num = Add(num);
+	printf("%d\n", num);
 	return 0;
 }
