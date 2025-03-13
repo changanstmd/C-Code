eb7f7a70d3f24236c13e496016818495efa28333
changanstmd <1209625009@qq.com>
2025-03-14 01:35:39 +0800

    3_14

--- 2025_3_1/2025_3_1/test3_4.c
+++ 2025_3_1/2025_3_1/test3_4.c
@@ -148,3 +148,121 @@
 //	printf("%lf\n", sum);
 //	return 0;
 //}
+
+
+
+//求十个整数中的最大值
+//int main()
+//{
+//	int arr[10] = {0};
+//	int i = 0;
+//	for (i = 0;i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int max = arr[0];
+//	for (i = 1;i <= 10;i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d", max);
+//}
+
+
+
+//在屏幕上打印9*9乘法表
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i < 10;i++)
+//	{
+//		int j = 0;
+//		for (j = 1;j < 1;j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		//%2d打印两位整数，即使一位整数，也用空格填充，两个右对其，-2%d两位左对齐
+//		printf("\n");
+//	}
+//	return 0;
+//}0
+
+
+//逗号表达式：从左往右计算各个表达式的值，规定最后一个表达式的值为整个逗号表达式的值
+//函数：打印n*n乘法表
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1;i <= n;i++)
+//	{
+//		int j = 0;
+//		for (j = 1;j <= i;j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+
+
+
+//把函数处理结果的两个数据返回给主调函数
+//1.return无法传两个数据
+
+//2.形参用数组
+//void test(int arr[])//int* arr
+////把首元素的地址传过去
+//{
+//	arr[0] = 3;//arr[0]--> *(arr+0)
+//	arr[1] = 4;//arr[1]--> *(arr+1)
+//	//首元素地址+1
+//}
+//
+//int main()
+//{
+//	int arr[2] = { 0 };
+//	test(arr);
+//	return 0;
+//}
+
+//3.形参用两个指针
+//void test(int* px, int* py)
+//{
+//	*px = 3;
+//	*py = 4;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	test(&a, &b);
+//	return 0;
+//}
+
+//4.形参用两个全局变量
+//int a = 0;
+//int b = 0;
+//
+//void test()
+//{
+//	a = 3;
+//	b = 4;
+//}
+//
+//int main()
+//{
+//	test();
+//	printf("a=%d b=%d\n", a, b);
+//}
