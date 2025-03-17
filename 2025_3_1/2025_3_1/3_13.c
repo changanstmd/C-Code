42ec3ffe00152648d41e0a0ebbcff68c3719b3ba
changanstmd <1209625009@qq.com>
2025-03-17 22:50:37 +0800

    3_17

--- 2025_3_1/2025_3_1/3_13.c
+++ 2025_3_1/2025_3_1/3_13.c
@@ -62,35 +62,49 @@
 //数组传参时，形参有两种写法
 //1.数组
 //2.指针
-void bubble_sort(int arr[],int sz)
-{
-	int i = 0;
-	for (i = 0;i < sz - 1;i++)
-	{
-		int j = 0;
-		for (j = 0;j < sz - 1 - i;j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				int temp = arr[j + 1];
-				arr[j + 1] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
-}
+//void bubble_sort(int arr[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr ,sz);
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
 
-int main()
-{
-	//把数组排成升序
-	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	bubble_sort(arr ,sz);
-	//数组名本质是首元素的地址
-	int i = 0;
-	for (i = 0;i < sz;i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
-}
+
+//数组名确实能表示首元素的地址
+//但是有两个例外
+//1.sizeof(数组名)，这里的数组名表示整个数组，计算整个数组的大小，单位是字节
+//2.&数组名，这里数组名表示整个数组，取出的是整个数组的地址
+//二维数组数组名的理解
+//int main()
+//{
+//	int arr[3][4];
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+//	printf("%d", sizeof(arr[0]) / sizeof(arr[0][0]));
+//	arr;
+//	二维数组的数组名也表示首行一维数组的地址
+//	return 0;
+//}
